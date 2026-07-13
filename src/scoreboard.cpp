cbbbb7ac03e536a8d7ce4584450e8548d8ccb8af
stevelacybrazil <75767576+stevelacybrazil@users.noreply.github.com>
2026-07-12 20:52:55 -0700

    Update scoreboard.cpp

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -15,7 +15,7 @@ int calculateTotal(const int scores[], int size) {
     int total = 0;
 
     for (int i = 0; i < size; i++) {
-        total += socres[i]
+        total += scores[i]
     }
 
     return total;
